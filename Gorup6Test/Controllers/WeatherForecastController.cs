057480cfd6a89d789360eed806a3465c05a10bd1
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-09-10 15:44:02 +0200

    Angeliki test

--- Gorup6Test/Controllers/WeatherForecastController.cs
+++ Gorup6Test/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Gorup6Test.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Angeliki test"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
